2f985e24321754a0175b347aa4a19eb286322069
7ping <z7ping@outlook.com>
2026-09-03 18:02:02 +0800

    feat(ui): 导出统一基础组件

---
+++ packages/web/src/components/ui/index.ts
@@ -0,0 +1,19 @@
+export {
+  Button,
+  Disclosure,
+  IconButton,
+  Input,
+  Select,
+  StatusBadge,
+  Textarea,
+  Toolbar,
+  ToolbarGroup,
+} from './Primitives'
+export type {
+  ButtonProps,
+  ButtonSize,
+  ButtonVariant,
+  DisclosureProps,
+  IconButtonProps,
+  StatusTone,
+} from './Primitives'
